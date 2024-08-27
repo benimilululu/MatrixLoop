708ec5f84ef86e6ad241427cc677627fc19fa6b7
benimilululu <jovanovik.martin2@yahoo.com>
2024-08-27 22:49:32 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -25,31 +25,108 @@ function App() {
     let newDirection = currDirection;
 
     let possibleTurn = /^[A-Z+]$/.test(currChar);
-    
+
     let shouldStop = currChar === 's';
 
     if (possibleTurn) {
+      //! direction is right ->
       if (currDirection === 'r') {
-        if (matrix[currColInd + 1][currRowInd] != ' ') {
-          nextColInd += 1;
-          newDirection = 'd';
+        //? if the curr col is the fist one ->
+        if (currColInd === 0) {
+          //? if we have valid character at position bellow ->
+          if (matrix[currColInd + 1][currRowInd] != ' ') {
+            nextColInd += 1;
+            newDirection = 'd';
+          }
+
+          //? if we are at the bottom ->
+        } else if (currColInd === matrixColLength - 1) {
+          if (matrix[currColInd - 1][currRowInd] != ' ') {
+            nextColInd -= 1;
+            newDirection = 'd';
+          }
+
+          // ? if we are not on top or bottom ->
+        } else {
+          if (matrix[currColInd - 1][currRowInd] != ' ') {
+            nextColInd -= 1;
+            newDirection = 'd';
+          } else if (matrix[currColInd + 1][currRowInd] != ' ') {
+            nextColInd += 1;
+            newDirection = 'd';
+          }
         }
+
+        //! direction is down ->
       } else if (currDirection === 'd') {
-        if (matrix[currColInd][currRowInd - 1] != ' ') {
-          nextRowInd -= 1;
-          newDirection = 'l';
+        //? if we are on the right end ->
+        if (currRowInd === matrixRowLength - 1) {
+          if (matrix[currColInd][currRowInd - 1] != ' ') {
+            nextRowInd -= 1;
+            newDirection = 'l';
+          }
+
+          // ? if we are on the starting left side
+        } else if (currRowInd === 0) {
+          if (matrix[currColInd][currRowInd + 1] != ' ') {
+            nextRowInd += 1;
+            newDirection = 'l';
+          }
+        } else {
+          if (matrix[currColInd][currRowInd + 1] != ' ') {
+            nextRowInd += 1;
+            newDirection = 'l';
+          } else if (matrix[currColInd][currRowInd - 1] != ' ') {
+            nextRowInd -= 1;
+            newDirection = 'l';
+          }
         }
+
+        // ! direction left ->
       } else if (currDirection === 'l') {
-        if (matrix[currColInd - 1][currRowInd] != ' ') {
-          nextColInd -= 1;
-          newDirection = 'up';
-        } else if (matrix[currColInd + 1][currRowInd] != ' ') {
-          nextColInd += 1;
-          newDirection = 'd';
+        // ? if we are on the bottom ->
+        if (currColInd === matrixColLength) {
+          if (matrix[currColInd + 1][currRowInd] != ' ') {
+            nextColInd += 1;
+            newDirection = 'd';
+          }
+
+          // ? top
+        } else if (currColInd === 0) {
+          if (matrix[currColInd + 1][currRowInd] != ' ') {
+            nextColInd += 1;
+            newDirection = 'd';
+          }
+        } else {
+          if (matrix[currColInd - 1][currRowInd] != ' ') {
+            nextColInd -= 1;
+            newDirection = 'up';
+          } else if (matrix[currColInd + 1][currRowInd] != ' ') {
+            nextColInd += 1;
+            newDirection = 'd';
+          }
         }
+
+        // ! Direction up ->
       } else if (currDirection === 'up') {
-        if (matrix[currColInd][currRowInd - 1] != ' ') {
-          newDirection = 'l';
+        // ? if we are on the starting left side ->
+        if (currRowInd === 0) {
+          if (matrix[currColInd][currRowInd + 1] != ' ') {
+            newDirection = 'r';
+          }
+
+          // ? if we are on right end of the row ->
+        } else if (currRowInd === matrixRowLength - 1) {
+          if (matrix[currColInd][currRowInd - 1] != ' ') {
+            newDirection = 'l';
+          }
+        } else {
+          if (matrix[currColInd][currRowInd + 1] != ' ') {
+            newDirection = 'r';
+          }
+          if (matrix[currColInd][currRowInd - 1] != ' ') {
+            newDirection = 'l';
+          }
         }
       }
     }
@@ -110,7 +187,14 @@ function App() {
   let path = [];
   let letters = [];
 
+  let count = 0;
+
   while (true) {
+      count++;
+    
+    if (count >= 66) {
+        break; 
+    }
     const { nextRowInd, nextColInd, shouldStop, newDirection } =
       findRightPathOfMatrix(matrix, currColInd, currRowInd, currDirection);
 
@@ -135,12 +219,6 @@ function App() {
 
   console.log(path);
   console.log(letters);
-
-  const inputText = `>---A---+
-                             |
-                     +-U-+   C
-                     |   |   |
-                     s   C---+`;
 
   return (
     <>
